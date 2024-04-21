d70f13c715dbe4664f1e903391968f920893c19e
CoutCout <72403468+ArthurCouturier@users.noreply.github.com>
2024-04-21 20:32:49 +0200

    added securization of the gellAll user endpoints

--- src/main/java/cout/dev/projetcuisine/controllers/UserController.java
+++ src/main/java/cout/dev/projetcuisine/controllers/UserController.java
@@ -2,6 +2,7 @@ package cout.dev.projetcuisine.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,9 @@ import cout.dev.projetcuisine.services.UserService;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    @Value("${app.admin.secret}")
+    private String appAdminSecret;
     
     private final UserService userService;
     private GoogleTokenVerifierService tokenVerifierService;
@@ -49,14 +53,19 @@ public class UserController {
     }
 
     @GetMapping("/getAll")
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers(@RequestHeader("Authorization") String authorizationHeader) {
+        if (!authorizationHeader.equals(appAdminSecret)) {
+            return null;
+        }
         return userService.getAllUsers();
     }
 
     @GetMapping("/google/getAll")
-    public List<GoogleUser> getAllGoogleUsers() {
+    public List<GoogleUser> getAllGoogleUsers(@RequestHeader("Authorization") String authorizationHeader) {
+        if (!authorizationHeader.equals(appAdminSecret)) {
+            return null;
+        }
         return userService.getAllGoogleUsers();
     }
-    
     
 }
